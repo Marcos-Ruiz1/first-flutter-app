9df04123ae2513bfce55288c60ba8e3c053ec405
Marcos-Ruiz1 <marcos.zazuetaruiz1@gmail.com>
2026-06-29 17:35:49 -0700

    WIP: creating start button

--- lib/UI/home_screen.dart
+++ lib/UI/home_screen.dart
@@ -8,12 +8,48 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.purpleAccent,
+      color: Color(0xFF0A1929),
       alignment: Alignment(0, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [ElevatedButton(onPressed: () {}, child: Text("Start"))],
+        children: [
+          buildButton(),
+        ],
       ),
+    );
+  }
+
+  
+  Widget buildButton(){
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFF0A1929),
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(
+          color: Color(0xFFC850C0),  // Border color
+          width: 2,  // Border thickness
+        ),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(100),
+          onTap: () {
+            print("button pressed");
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Start",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
